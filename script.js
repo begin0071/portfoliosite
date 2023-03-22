8809cdd76e17c6d2fe8616d97accb42f2ab16881
begin0071 <m.khaki610@gmail.com>
2023-03-22 17:11:09 -0500

    added light/dark mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const toggleSwitch = document.getElementById('toggle-switch');
+const themeLink = document.getElementById('theme-link');
 
+toggleSwitch.addEventListener('click', function() {
+  if (themeLink.getAttribute('href') == 'style-light.css') {
+    themeLink.setAttribute('href', 'style-dark.css');
+  } else {
+    themeLink.setAttribute('href', 'style-light.css');
+  }
+});
